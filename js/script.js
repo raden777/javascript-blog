cae80940bb73f382c5acaac3c4cc529952235cf2
Maciek Uzi <uzi555555@gmail.com>
2023-03-30 23:27:07 +0200

    ADD CLASS ACTIVE IN targetArcticle

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
 'use strict';
 
 function titleClickHandler(event) {
-  const clickedElement = this {
-    clickedElement.classList.add('active');
-  }
+  event.preventDefault();
+  const clickedElement = this;
   console.log('Link was clicked!');
 
-  /* remove class 'active' from all article links  */
+  /* [done]remove class 'active' from all article links  */
 
   const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -16,22 +15,29 @@ function titleClickHandler(event) {
 
   /* add class 'active' to the clicked link */
 
-  console.log('clickedElement:', clickedElement);
-}
+  clickedElement.classList.add('active');
+  console.log('ClickedElement:', clickedElement);
 
   /* remove class 'active' from all articles */
 
-  const activeArticles = document.querySelectorAll('.post.active');
+  const activeArticles = document.querySelectorAll('.posts .active');
 
   for (let activeArticle of activeArticles) {
     activeArticle.classList.remove('active');
   }
 
   /* get 'href' attribute from the clicked link */
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log(articleSelector);
 
   /* find the correct article using the selector (value of 'href' attribute) */
 
+  const targetArticle = document.querySelector(articleSelector);
+  console.log(targetArticle);
+
   /* add class 'active' to the correct article */
+
+  targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
